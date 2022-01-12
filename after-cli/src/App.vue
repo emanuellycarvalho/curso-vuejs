99c11ec6e11f2edc6a6227de715c705c5e59ee6c
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2022-01-11 23:56:28 -0300

    Configurando o template

--- after-cli/src/App.vue
+++ after-cli/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="https://iconape.com/wp-content/png_logo_vector/git-icon.png" class="d-inline-block align-top" alt="">
+            Git Issues
+        </a>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'App',
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app .navbar a{
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 30px;
+    font-weight: 500;
+  }
+
+  #app .navbar a img {
+    width: 40px;
+    height: 40px;
+    margin-top: 3px;
+  }
 </style>
--- after-cli/src/main.js
+++ after-cli/src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
-})
+  render: h => h(App),
+});
